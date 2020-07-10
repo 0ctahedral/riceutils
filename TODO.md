d6dd7fc798619a790951d6cb69d6fb669b21c95f
xenone <rosenkranz1999@gmail.com>
2020-07-10 18:47:16 -0400

    terminal color sequences, in and out of tmux

--- TODO.md
+++ TODO.md
@@ -6,7 +6,9 @@
 1. Parse pallet
  - [x] create file format
  - [x] parse that format
- - [ ] print out all pallets
+3. Print terminal colors
+ - [ ] translate pallet to esc codes
+ - [ ] add pre/suffix
 2. Apply Rice
  - [ ] create template convention
  - [ ] replace vars in template
--- main.sh
+++ main.sh
@@ -33,9 +33,78 @@ parse_pallet() {
     ' "$file"
 }
 
+apply_pallet() {
+    pal="${1}"
+    trg="$2"
+
+    {
+        echo "$pal"
+        echo "__ENDPALLET"
+        cat "$trg"
+    } | awk '
+        {print}
+    '
+}
+
+print_term() {
+    pal="${1}"
+
+    [ -n "$TMUX" ] && {
+
+        esc='\033Ptmux;\033\033]'
+        cesc='\007\033\\'
+    }
+
+    : "${esc=\033]}"
+    : "${cesc=\007}"
+
+    clr="$2"
+    echo "$pal" | grep "$clr" | awk -v esc="${esc}" -v cesc="${cesc}" '
+        $1 == "bg" {
+            printf "%s11;%s%s\n", esc, $2, cesc
+            printf "%s4;0;%s%s\n", esc, $2, cesc
+        }
+        $1 == "bg_alt" {
+            printf "%s;8;%s%s\n", esc, $2, cesc
+        }
+        $1 == "primay" {
+            printf "%s4;1;%s%s\n", esc, $2, cesc
+            printf "%s4;9;%s%s\n", esc, $2, cesc
+        }
+        $1 == "secondary" {
+            printf "%s4;2;%s%s\n", esc, $2, cesc
+            printf "%s4;10;%s%s\n", esc, $2, cesc
+        }
+        $1 == "alert" {
+            printf "%s4;3;%s%s\n", esc, $2, cesc
+            printf "%s4;11;%s%s\n", esc, $2, cesc
+        }
+        $1 == "cursor" {
+            printf "%s4;4;%s%s\n", esc, $2, cesc
+            printf "%s4;12;%s%s\n", esc, $2, cesc
+            printf "%s12;%s%s\n", esc, $2, cesc
+        }
+        $1 == "fill" {
+            printf "%s4;5;%s%s\n", esc, $2, cesc
+            printf "%s4;6;%s%s\n", esc, $2, cesc
+            printf "%s4;13;%s%s\n", esc, $2, cesc
+            printf "%s4;14;%s%s\n", esc, $2, cesc
+        }
+        $1 == "fg" {
+            printf "%s10;%s%s\n", esc, $2, cesc
+            printf "%s4;7;%s%s\n",esc,  $2, cesc
+        }
+        $1 == "fg_alt" {
+            printf "%s17;%s%s\n", esc, $2, cesc
+            printf "%s4;15;%s%s\n", esc, $2, cesc
+        }
+    '
+}
+
 main() {
     pallet=$(parse_pallet "$PALLET_PATH/$PALLET_FILE")
-    echo "$pallet"
+
+    print_term "$pallet"
 }
 
 main "${@}"
