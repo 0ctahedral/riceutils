735136b989f895d3d25d8ae7ee926d927f4fe8f7
xenone <rosenkranz1999@gmail.com>
2020-08-01 16:32:07 -0400

    added color printing and usage for pallet cmd

--- cmd/pallet/main.go
+++ cmd/pallet/main.go
@@ -8,33 +8,109 @@ import (
 	"github.com/xen0ne/riceutils/pkg/term"
 )
 
+var args = make(map[string]*string)
 
-// cmdline args
-func usage() {
-	use := `pallet <pallet>
+// var h = flag.Bool("help", false, "help menu")
+
+func arginit() {
+	args["x"] = flag.String("hex", "", "")
+	flag.StringVar(args["x"], "x", "", "")
+	args["r"] = flag.String("rgb", "", "")
+	flag.StringVar(args["r"], "r", "", "")
+	args["s"] = flag.String("hsv", "", "")
+	flag.StringVar(args["s"], "s", "", "")
+
+	flag.Usage = func() {
+		use := `usage: pallet [options...] [pallet]
+
 supplying no arguments will apply the current pallet to the terminal
+supplying only a pallet will apply the given pallet to the terminal
+
+general options:
 -h | --help prints this message
--n | --new creates a new pallet with the given name
--e | --edit changes a value in the pallet (idk how to do that yet)
--p | --picker opens a gui for editing the given pallet
+
+printing options:
+by using a -- to supply no arguments will print all colors from the pallet
+-x | --hex prints the given color in hex format
+-r | --rgb prints the given color in comma separated red, green, and blue
+-s | --hsv prints the given color in comma separated hue, saturation, and value
 	`
-	fmt.Println(use)
+		fmt.Fprintf(flag.CommandLine.Output(), "%s\n", use)
+	}
 }
+
 
 func main() {
 
-	//var args map[string]*bool
+	arginit()
 	
 	// n := flag.Bool("new", false, "name of new pallet")
-	h := flag.Bool("help", false, "help menu")
 	flag.Parse()
 
 	if len(os.Args) == 1 {
-		simp := color.DefaultPallet()
-		fmt.Printf("%s", term.EscPallet(simp, term.Stdmap))
+		def := color.DefaultPallet()
+		fmt.Printf("%s", term.EscPallet(def, term.Stdmap))
+		os.Exit(0)
 	}
 
-	if (*h) {
-		usage()
+	if *args["x"] != "" {
+		if (*args["x"] == "-" || *args["x"] == "--") {
+			PrintPallet(color.HexString)
+			os.Exit(0)
+		} else {
+			PrintColorFromPallet(*args["x"], color.HexString)
+		}
 	}
+
+	if *args["r"] != "" {
+		if (*args["r"] == "-" || *args["r"] == "--") {
+			PrintPallet(color.RgbString255)
+			os.Exit(0)
+		} else {
+			PrintColorFromPallet(*args["r"], color.RgbString255)
+		}
+	}
+
+	if *args["s"] != "" {
+		if (*args["s"] == "-" || *args["s"] == "--") {
+			PrintPallet(color.HsvString)
+			os.Exit(0)
+		} else {
+			PrintColorFromPallet(*args["s"], color.HsvString)
+		}
+	}
+}
+
+func PrintPallet(a func(*color.Color) string) {
+	var p *color.Pallet
+
+	if len(flag.Args()) == 0 {
+		p = color.DefaultPallet()
+	} else {
+		// read the pallet from file
+		p = color.CleanPallet()
+	}
+
+
+	for _, v := range p.Iter() {
+		fmt.Println(a(v))
+	}
+
+}
+
+func PrintColorFromPallet(str string, a func(*color.Color) string) {
+	var p *color.Pallet
+
+	if len(flag.Args()) == 0 {
+		p = color.DefaultPallet()
+	} else {
+		p = color.CleanPallet()
+	}
+
+	if c, ok := p.Iter()[str]; ok {
+		fmt.Println(a(c))
+		os.Exit(0)
+	}
+	fmt.Fprintf(os.Stderr, "%s is not a valid color\n", str)
+	os.Exit(1)
 }
